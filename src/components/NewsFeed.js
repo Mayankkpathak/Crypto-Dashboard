69a7508abef56cce58003b8faf7e96a7bc321508
Mayankkpathak <mayankpathak005@gmail.com>
2022-09-27 01:04:31 +0530

    forth commit

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -1,7 +1,41 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
 const NewsFeed = () => {
+  const [articles, setArticles] = useState(null)
+
+  useEffect(() =>  {
+
+
+const options = {
+  method: 'GET',
+  url: 'https://crypto-news-live3.p.rapidapi.com/news',
+  headers: {
+    'X-RapidAPI-Key': '0cffbe51f9msh0529a1b15df46dep1bf40cjsn34b535b508bd',
+    'X-RapidAPI-Host': 'crypto-news-live3.p.rapidapi.com'
+  }
+}
+
+axios.request(options).then((response) => {
+	console.log(response.data)
+  setArticles(response.data)
+}).catch((error) => {
+	console.error(error)
+})
+
+  }, [])
+
+  console.log(articles)
+
+  const first7Articles = articles?.slice(0,7)
+  
   return (
     <div className="news-feed">
-     NewsFeed
+      <h2>News Feed</h2>
+     {first7Articles?.map((article, _index) => (
+     <div key={_index}>
+      <a href={article.url}><p>{article.title}</p></a>
+     </div>))}
     </div>
   )
 }
